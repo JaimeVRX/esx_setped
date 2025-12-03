c9e64539a08c5d7df54875c44ec9039c49ea35a1
Jaime <gertgart40@gmail.com>
2025-12-03 09:08:24 +0100

    Refactor config for allowed roles and favorite peds

--- esx_setped/config.lua
+++ esx_setped/config.lua
@@ -1,18 +1,52 @@
 Config = {}
 
-
 Config.AllowedRoles = {
-    "Staff",
-    "Admin",
-    "Moderator"
+    "staff",
+    "admin",
+    "mod"
 }
 
-
-Config.Peds = {
-    { label = "Poema", model = "a_c_puma" },
+Config.FavoritePeds = {
+    { label = "K", model = "a_m_m_fatlatin_01" },
     { label = "Panter", model = "a_c_panther" },
     { label = "Hond", model = "a_c_shepherd" },
     { label = "Kat", model = "a_c_cat_01" },
+    { label = "B", model = "s_m_y_clown_01" },
+    { label = "M", model = "a_f_m_beach_01" }
 }
 
 Config.Command = "setped"
+
+Config.AllPeds = {
+    { label = "g_m_m_armboss_01", model = "g_m_m_armboss_01" },
+    { label = "g_m_m_armlieut_01", model = "g_m_m_armlieut_01" },
+    { label = "g_m_m_armgoon_01", model = "g_m_m_armgoon_01" },
+    { label = "g_m_m_chemwork_01", model = "g_m_m_chemwork_01" },
+    { label = "g_m_m_cartelguards_01", model = "g_m_m_cartelguards_01" },
+    { label = "g_m_m_cartelguards_02", model = "g_m_m_cartelguards_02" },
+    { label = "g_m_y_armgoon_02", model = "g_m_y_armgoon_02" },
+    { label = "g_m_y_ballaeast_01", model = "g_m_y_ballaeast_01" },
+    { label = "g_m_y_ballaorig_01", model = "g_m_y_ballaorig_01" },
+    { label = "g_m_y_ballasout_01", model = "g_m_y_ballasout_01" },
+    { label = "g_m_y_famca_01", model = "g_m_y_famca_01" },
+    { label = "g_m_y_famdnf_01", model = "g_m_y_famdnf_01" },
+    { label = "g_m_y_famfor_01", model = "g_m_y_famfor_01" },
+    { label = "g_m_y_korlieut_01", model = "g_m_y_korlieut_01" },
+    { label = "g_m_m_mexboss_01", model = "g_m_m_mexboss_01" },
+    { label = "g_m_m_mexboss_02", model = "g_m_m_mexboss_02" },
+    { label = "g_m_y_mexgoon_01", model = "g_m_y_mexgoon_01" },
+    { label = "g_m_y_mexgoon_02", model = "g_m_y_mexgoon_02" },
+    { label = "g_m_y_mexgoon_03", model = "g_m_y_mexgoon_03" },
+    { label = "g_m_y_pologoon_01", model = "g_m_y_pologoon_01" },
+    { label = "g_m_y_pologoon_02", model = "g_m_y_pologoon_02" },
+    { label = "g_m_y_salvaboss_01", model = "g_m_y_salvaboss_01" },
+    { label = "g_m_y_salvagoon_01", model = "g_m_y_salvagoon_01" },
+    { label = "g_m_y_salvagoon_02", model = "g_m_y_salvagoon_02" },
+    { label = "g_m_y_salvagoon_03", model = "g_m_y_salvagoon_03" },
+    { label = "g_m_y_strpunk_01", model = "g_m_y_strpunk_01" },
+    { label = "g_m_y_strpunk_02", model = "g_m_y_strpunk_02" },
+    { label = "s_m_y_prisoner_01", model = "s_m_y_prisoner_01" },
+    { label = "s_m_m_prisguard_01", model = "s_m_m_prisguard_01" },
+
+    -- Add more peds here
+}
